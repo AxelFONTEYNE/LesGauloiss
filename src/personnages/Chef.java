b293f79494606c71e65233fc2f06c2216a0752df
AxelFONTEYNE <axfonteyne3@gmail.com>
2022-10-06 15:29:29 +0200

    tp1 LesGaulois last modif : 06/10/2022 15:29

---
+++ src/personnages/Chef.java
@@ -0,0 +1,17 @@
+package personnages;
+
+import Lieu.Village ;
+
+public class Chef {
+	private String nom;
+    private int force;
+    private int effetPotion = 1;
+    private Village village;
+
+    public Chef(String nom, int force, Village village) {
+        this.nom = nom;
+        this.force = force;
+        this.effetPotion = effetPotion;
+        this.village = village;
+    }
+}
